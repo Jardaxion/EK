3e56264cd15f24310e074b742c78ddb0ac5ba58d
Jardaxion <bainthecreator21@gmail.com>
2022-11-05 22:18:37 +0700

    Исправил почитать

--- js/main.js
+++ js/main.js
@@ -134,39 +134,11 @@ function visible (target) {
 
 };
 
-function changeColor(target){
-    // Все позиции элемента
-    var targetPosition = {
-        top: window.pageYOffset + target.getBoundingClientRect().top,
-        left: window.pageXOffset + target.getBoundingClientRect().left,
-        right: window.pageXOffset + target.getBoundingClientRect().right,
-        bottom: window.pageYOffset + target.getBoundingClientRect().bottom
-    },
-    // Получаем позиции окна
-    windowPosition = {
-        top: window.pageYOffset,
-        left: window.pageXOffset,
-        right: window.pageXOffset + document.documentElement.clientWidth,
-        bottom: window.pageYOffset + document.documentElement.clientHeight
-    };
-  
-    if (targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
-        targetPosition.top < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
-        targetPosition.right > windowPosition.left &&// Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
-        targetPosition.left < windowPosition.right) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
-            $('.nav__item').addClass('black');
-    } else {
-        $('.nav__item').removeClass('black');
-    }
-}
-
 function allFunctions(){
     visible(document.querySelector('.gallery'));
     visible(document.querySelector('.reviews'));
     visible(document.querySelector('.articles'));
     visible(document.querySelector('.partners'));
-
-    changeColor(document.querySelector('.lastTwo'));
 }
 
 function toggleMenu(){
@@ -191,7 +163,7 @@ function scrollHorizontally(e) {
 
 //Скролл "Почитать"
 function scrollPochi(){
-    let widthToPochitat = $(window).width()*3.6;
+    let widthToPochitat = $(window).width()*4.6;
 
     if(widthToPochitat <= $(window).scrollLeft()){
         $('.articles__title').css({
